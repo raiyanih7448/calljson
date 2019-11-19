73cdb9607eec7607ece8d608e6846a7b5bd0dbc6
raiyanih7448 <42166559+raiyanih7448@users.noreply.github.com>
2019-11-19 18:23:17 +0530

    Add files via upload

---
+++ VolleyResponseListener.java
@@ -0,0 +1,11 @@
+package midlalpro.midlalpro.midlalpro.Webservice;
+
+/**
+ * Created by Admin on 15-11-2017.
+ */
+
+public interface VolleyResponseListener {
+    void onError(String message);
+
+    void onResponse(Object response, WebCallid webcallid);
+}
---
+++ WebCallid.java
@@ -0,0 +1,48 @@
+package midlalpro.midlalpro.midlalpro.Webservice;
+
+/**
+ * Created by Ravi on 1/12/2018.
+ */
+
+public enum WebCallid {
+
+    UP,
+    DOWN,
+    CLOSE,
+    DONE,
+
+    LOGIN,
+    REGISTER,
+    SMS_VERIFY,
+    RESEND_OTP,
+    FORGOT_PASSWORD,
+    CATEGORY,
+    NEW_BUSINESS,
+    GOOGLEMAP,
+    GOOGLEPLACES,
+    GOOGLEPLACESDEATIL,
+    BUSINESSDETAIL,
+    LISTING_DETAIL,
+    GET_BUSINESS_DETAIL,
+    SAVE_CONTACT_INFO,
+    BUSINESS_STATUS,
+    GET_BUSINESS_LIST,
+    DELETE_BUSINESS,
+    COPY_BUSINESS, addPhotoCategory, addPhotoGallery, editBusiness, deletePhotos,
+    WORKING_HOURS,
+    DUPLICATE_EMAIL_CHECK,
+    EDIT_PROFILE,
+    CHANGE_PASSWORD,
+    CREATE_MENU,
+    DELETE_MENU,
+    CREATE_SECTION,
+    DELETESECTION,
+    CHANGEORDERMENULIST,
+    DELETEMENULIST,
+    CREATE_MENULIST,
+    BUSINESS_EDITOR,
+    DELETE_BUSINESS_EDITOR,
+    ADD_YOUTUBE_LINK,
+
+
+}
